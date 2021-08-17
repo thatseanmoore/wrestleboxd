7b2db84eb808bd511a66d505108d327c43a6b851
nmalcock <nmalcock97@gmail.com>
2021-08-17 13:53:44 -0400

    Added Index View for Course

---
+++ GolfTrack.WebMVC/Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using GolfTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GolfTrack.WebMVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            var model = new CourseListItem[0];
+            return View(model);
+        }
+    }
+}
