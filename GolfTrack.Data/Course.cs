a86a83bac9dc6a47e5d75cf6074bb9369e9e1cbd
nmalcock <nmalcock97@gmail.com>
2021-08-17 13:40:36 -0400

    Added Connection String and Modified Course Class

--- GolfTrack.Data/Course.cs
+++ GolfTrack.Data/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace GolfTrack.Data
 {
     public class Course
     {
+        [Key]
         public int CourseId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Holes { get; set; }
+        [Required]
         public string TypeOfCourse { get; set; }
+        [Required]
         public int Par { get; set; }
     }
 }
