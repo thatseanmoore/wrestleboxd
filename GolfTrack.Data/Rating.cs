e78d4d4efaf587be0f16052574e804e93696d2c2
nmalcock <nmalcock97@gmail.com>
2021-08-17 11:50:38 -0400

    Add project files.

---
+++ GolfTrack.Data/Rating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfTrack.Data
+{
+    public class Rating
+    {
+        public int RatingId { get; set; }
+        public int Stars { get; set; }
+        public string Review { get; set; }
+        public int CourseId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ GolfTrack.Data/Score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfTrack.Data
+{
+    public class Score
+    {
+        public int ScoreId { get; set; }
+        public int TotalScore { get; set; }
+        public DateTimeOffset RoundDate { get; set; }
+        public int CourseId { get; set; }
+        public Guid UserId { get; set; }
+
+    }
+}
